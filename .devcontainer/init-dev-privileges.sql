cf056a772f097aadac5fd21b7d6e1d2cdb3c155a
Dimitri Yatsenko <dimitri.yatsenko@gmail.com>
2025-09-13 22:15:46 -0500

    clean up

--- .devcontainer/init-dev-privileges.sql
+++ .devcontainer/init-dev-privileges.sql
@@ -1,7 +1,3 @@
 -- Ensure dev user exists and has broad privileges
 GRANT ALL PRIVILEGES ON *.* TO 'dev'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
-
--- Debug: Show final grants
-SELECT 'Privilege grants completed' AS debug_message;
-SHOW GRANTS FOR 'dev'@'%';
